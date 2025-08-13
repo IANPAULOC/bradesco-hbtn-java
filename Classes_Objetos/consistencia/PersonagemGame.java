9c8854868e886568db10859543ebe2be205ab5a6
Ian <ianpaulocruz@outlook.com>
2025-08-13 15:09:31 -0300

    4. Consistencia

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,59 @@
+public class PersonagemGame{
+
+    private String nome;
+    private int saudeAtual;
+    private String status;
+
+    public PersonagemGame(String nome, int saudeAtual, String status) {
+        this.nome = nome;
+        setSaudeAtual(saudeAtual);
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (!(nome == null || nome.isEmpty())){
+            this.nome = nome;
+        }
+
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0){
+            status = "vivo";
+        }
+        if (this.saudeAtual == 0){
+            status = "morto";
+        }
+
+    }
+
+    public void tomarDano (int quantidadeDeDano){
+        if (getSaudeAtual() - quantidadeDeDano <= 0){
+            setSaudeAtual(0);
+        }else {
+            setSaudeAtual(saudeAtual - quantidadeDeDano);
+        }
+    }
+
+    public void receberCura (int quantidadeDeCura){
+        if (getSaudeAtual() + quantidadeDeCura >= 100){
+            setSaudeAtual(100);
+        }else {
+            setSaudeAtual(saudeAtual + quantidadeDeCura);
+        }
+    }
+
+}
+

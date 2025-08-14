0a46717c89e8a8643ac657eb11678bed25d715f3
Ian <ianpaulocruz@outlook.com>
2025-08-13 23:41:18 -0300

    4.retangulo completo

---
+++ Heranca/retangulo_completo/Retangulo.java
@@ -0,0 +1,39 @@
+import static java.lang.String.format;
+
+public class Retangulo extends FormaGeometrica {
+    private double largura;
+    private double altura;
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if (largura < 0){
+           throw new java.lang.IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }else {
+        this.largura = largura;
+        }
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura < 0) {
+            throw new java.lang.IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        } else {
+            this.altura = altura;
+        }
+    }
+
+    @Override
+    public double area(){
+        return getAltura()*getLargura();
+    }
+
+    public String toString(){
+        return "[Retangulo] " + String.format(String.valueOf(getLargura())) + " / " + String.format(String.valueOf(getAltura())) ;
+    }
+}

a8bf29209ba6e24a8eb00e397ee895cf639fbbaa
Ian <ianpaulocruz@outlook.com>
2025-08-14 00:06:14 -0300

    6.abstrata

---
+++ Heranca/abstrata/Quadrado.java
@@ -0,0 +1,24 @@
+public class Quadrado extends Retangulo{
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if (lado < 0){
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        }else {
+            this.lado = lado;
+        }
+    }
+
+    @Override
+    public double area(){
+        return getLado()*getLado();
+    }
+    @Override
+    public String toString(){
+        return String.format("[Quadrado] %.2f", lado);
+    }
+}

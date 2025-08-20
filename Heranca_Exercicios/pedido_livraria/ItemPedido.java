f92d320ac3611547b2ec4eafe768c2f253c5459f
Ian <ianpaulocruz@outlook.com>
2025-08-20 18:17:50 -0300

    0. Pedido livraria

---
+++ Heranca_Exercicios/pedido_livraria/ItemPedido.java
@@ -0,0 +1,22 @@
+import produtos.Livro;
+import produtos.Produto;
+
+public class ItemPedido {
+
+    private Produto produto;
+    private int quantidade;
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+}
---
+++ Heranca_Exercicios/pedido_livraria/produtos/Produto.java
@@ -0,0 +1,23 @@
+package produtos;
+
+public abstract class Produto {
+
+    private String titulo;
+    private int ano;
+    private String pais;
+    private double precoBruto;
+
+    public double getPrecoBruto() {
+        return precoBruto;
+    }
+
+    public Produto(String titulo, int ano, String pais, double precoBruto) {
+        this.titulo = titulo;
+        this.ano = ano;
+        this.pais = pais;
+        this.precoBruto = precoBruto;
+    }
+
+    public abstract double obterPrecoLiquido();
+
+}

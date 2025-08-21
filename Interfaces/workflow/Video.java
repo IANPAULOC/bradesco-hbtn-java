669f656baedc14b9c707ea4f73d38d5f2148e291
Ian <ianpaulocruz@outlook.com>
2025-08-21 11:11:28 -0300

    o. logging interface

---
+++ Interfaces/workflow/Video.java
@@ -0,0 +1,26 @@
+public class Video {
+
+    private String arquivo;
+    private FormatoVideo formato;
+
+    public String getArquivo() {
+        return arquivo;
+    }
+
+    public void setArquivo(String arquivo) {
+        this.arquivo = arquivo;
+    }
+
+    public FormatoVideo getFormato() {
+        return formato;
+    }
+
+    public void setFormatoVideo(FormatoVideo formatoVideo) {
+        this.formato = formatoVideo;
+    }
+
+    public Video(String arquivo, FormatoVideo formato) {
+        this.arquivo = arquivo;
+        this.formato = formato;
+    }
+}

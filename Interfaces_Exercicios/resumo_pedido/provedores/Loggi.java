52381c86552d18d175a158d89b8fd79bfde2d3f9
Ian <ianpaulocruz@outlook.com>
2025-09-01 23:22:12 -0300

    1. frete

---
+++ Interfaces_Exercicios/resumo_pedido/provedores/Loggi.java
@@ -0,0 +1,25 @@
+package provedores;
+
+import provedores.Frete;
+import provedores.ProvedorFrete;
+import provedores.TipoProvedorFrete;
+
+public class Loggi implements ProvedorFrete {
+    @Override
+    public Frete calcularFrete(double peso, double valor) {
+        Frete frete = new Frete();
+
+        if (peso > 5000) {
+            frete.setValor(0.12*valor);
+        } else {
+            frete.setValor(0.04*valor);
+        }
+
+        return frete;
+    }
+
+    @Override
+    public TipoProvedorFrete obterTipoProvedorFrete() {
+        return TipoProvedorFrete.LOGGI;
+    }
+}

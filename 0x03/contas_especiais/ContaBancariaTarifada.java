0419752957effc76151f7dcaeac7ce268b20338c
Ian <ianpaulocruz@outlook.com>
2025-09-04 11:05:43 -0300

    Conta especial

---
+++ 0x03/contas_especiais/ContaBancariaTarifada.java
@@ -0,0 +1,26 @@
+import contas_especiais.exceptions.OperacaoInvalidaException;
+
+public class ContaBancariaTarifada extends ContaBancariaBasica{
+    private int quantidadeTransacoes;
+
+    public ContaBancariaTarifada(String numeracao, double taxaJurosAnual) {
+        super(numeracao, taxaJurosAnual);
+    }
+
+    public int getQuantidadeTransacoes() {
+        return quantidadeTransacoes;
+    }
+
+
+    @Override
+    public void sacar(double valor) throws OperacaoInvalidaException {
+        super.sacar(valor);
+        setSaldo(getSaldo() - 0.10);
+    }
+
+    @Override
+    public void depositar(double valor) throws OperacaoInvalidaException {
+        super.depositar(valor);
+        setSaldo(getSaldo() - 0.10);
+    }
+}

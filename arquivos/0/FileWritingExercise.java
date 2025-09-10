e321698d68515231d733c554bb141e4feeda183b
Ian <ianpaulocruz@outlook.com>
2025-09-10 00:43:52 -0300

    0.Escrevendo arquivos

---
+++ arquivos/0/FileWritingExercise.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileWritingExercise {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        // Solicita o nome do arquivo
+        System.out.print("Digite o nome do arquivo (com extensão .txt): ");
+        String fileName = scanner.nextLine();
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName , true))) {
+            boolean botaoSaida = false;
+            while (!botaoSaida){
+                System.out.println("Digite várias linhas de texto, caso deseje finalizar digite sair");
+                String line = scanner.nextLine();
+                if (line.equals(String.valueOf("sair"))){
+                    botaoSaida = true;
+                }else {
+                    bw.write(line + "\n");
+                }
+            }
+        }catch (IOException e){
+            e.getMessage();
+        }
+
+        scanner.close();
+    }
+}

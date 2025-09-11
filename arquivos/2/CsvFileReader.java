6b3a30001f908f81a9ae3be846193a88ae1dbc97
Ian <ianpaulocruz@outlook.com>
2025-09-10 23:48:01 -0300

    2.arquivos csv

---
+++ arquivos/2/CsvFileReader.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CsvFileReader {
+    public static void main(String[] args) {
+
+        String fileName = "C:\\Users\\Ian\\Repositorios github\\bradesco-hbtn-java\\arquivos\\2\\funcionarios.csv"; // Nome do arquivo fixo para leitura
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))){
+            while(true){
+                String line = bufferedReader.readLine();
+                if (line == null){
+                    System.out.println("Leitura do arquivo concluída.");
+                    break;
+                }
+                String[] separacao = line.toString().split(",");
+                String nome = separacao[0];
+                String idade = separacao[1];
+                String dep = separacao[2];
+                String sal = separacao[3];
+                System.out.println("Funcionário: " + nome );
+                System.out.println("Idade: " + idade );
+                System.out.println("Departamento: " + dep );
+                System.out.println("Salarial: " + sal );
+                System.out.println("------------------------");
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Erro " + e.getMessage());
+        }
+
+    }
+}

59728ca75f0b2154603cb8c441d669aad7c456ac
Ian <ianpaulocruz@outlook.com>
2025-09-15 09:04:30 -0300

    2.Complexidade

--- arquivos/3/Estoque.java
+++ arquivos/3/Estoque.java
@@ -1,18 +1,41 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
+import java.io.*;
+
 
 public class Estoque {
 
     String path;
+    int id;
 
     public Estoque(String path) {
         this.path = path;
     }
 
-
     public void adicionarProduto(String nome, int quantidade, double preco) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String[] pegaId = null;
+            int id = 0;
+            while (true){
+                String line = bufferedReader.readLine();
+                if (line != null){
+                    pegaId = line.split(",");
+                    id =  Integer.parseInt(pegaId[0]);
+                }else {
+                    acrescentaId(id);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Erro " + e.getMessage());
+        }
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path , true))) {
+            bufferedWriter.write("\n" + id + "," + nome + "," + quantidade + "," + preco);
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao salvar o arquivo: " + e.getMessage());
+        }
+    }
+
+    private void acrescentaId(int id) {
+        this.id = id +1;
     }
 
     public void excluirProduto(int idExcluir) {

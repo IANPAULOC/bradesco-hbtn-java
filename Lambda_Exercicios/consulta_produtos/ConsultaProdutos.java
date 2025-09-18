279018dc421dd6aeb9786b81d0076d5ed9c813ac
Ian <ianpaulocruz@outlook.com>
2025-09-18 15:28:10 -0300

    0. consulta produtos

---
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsultaProdutos {
+
+    public static List<Produto> filtrar (List<Produto> produtos , CriterioFiltro criterioFiltro){
+        List<Produto> filtrados = new ArrayList<>();
+        for (Produto produto : produtos){
+            if (criterioFiltro.testar(produto)){
+                filtrados.add(produto);
+            }
+        }
+        return filtrados;
+    }
+}
